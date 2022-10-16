71d9a9a624769ba3a199bc04a3de5c1066de180d
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-10-16 19:05:07 +1100

    Update Device_IP_Address_CSV_Allocator.cpp

--- Device_IP_Address_CSV_Allocator.cpp
+++ Device_IP_Address_CSV_Allocator.cpp
@@ -1,1 +1,14 @@
+// Device_IP_Address_CSV_Allocator.cpp
 
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+int main()
+{
+    std::ofstream output_file;
+    output_file.open("Test.csv");\
+    output_file << "123" << "," << "456" << "\n";
+    output_file.close();
+    return 0;
+}
