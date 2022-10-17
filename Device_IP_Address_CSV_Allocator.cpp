cacd65398dbc3a91203886fba75f0f19cc6a117c
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-10-17 22:41:26 +1100

    Update Device_IP_Address_CSV_Allocator.cpp

--- Device_IP_Address_CSV_Allocator.cpp
+++ Device_IP_Address_CSV_Allocator.cpp
@@ -7,8 +7,10 @@
 int main()
 {
     std::ofstream output_file;
-    output_file.open("Test.csv");\
-    output_file << "123" << "," << "456" << "\n";
+    output_file.open("Test.csv");
+    output_file << "Section 1" << "," << " " << "," << "Section 2" << "\n";
+    output_file << "Subnet 1" << "," << " " << "," << "Subnet 1" << "," << " " << "\n";
+    output_file << "Device" << "," << "IP Address Placeholder" << "," << "Device" << "," << "IP Address Placeholder" << "\n";
     output_file.close();
     return 0;
 }
