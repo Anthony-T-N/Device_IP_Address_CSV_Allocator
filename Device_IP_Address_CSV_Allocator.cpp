3ba1ae3b78b04ced077153c99d4b17d10762eee4
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-10-18 22:01:33 +1100

    Update Device_IP_Address_CSV_Allocator.cpp

--- Device_IP_Address_CSV_Allocator.cpp
+++ Device_IP_Address_CSV_Allocator.cpp
@@ -2,15 +2,20 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <filesystem>
 
 int main()
 {
+    std::string user_input;
+    std::cout << "> ";
+    std::getline(std::cin, user_input);
+
     std::ofstream output_file;
     output_file.open("Test.csv");
     output_file << "Section 1" << "," << " " << "," << "Section 2" << "\n";
     output_file << "Subnet 1" << "," << " " << "," << "Subnet 1" << "," << " " << "\n";
-    output_file << "Device" << "," << "IP Address Placeholder" << "," << "Device" << "," << "IP Address Placeholder" << "\n";
+    output_file << "Device" << "," << user_input << "," << "Device" << "," << user_input << "\n";
     output_file.close();
     return 0;
 }
